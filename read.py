ad06833bb52b087b6c1c2a983dc19d1886f65c22
Boyu Wu <rhetoric28569@gmail.com>
2020-09-07 15:30:07 +0800

    filter

--- read.py
+++ read.py
@@ -17,3 +17,14 @@ for d in data :
 	sum_len = sum_len + len(d)
 
 print('留言的平均長度是',sum_len/len(data))	
+
+
+
+new =[]  #挑選
+for d in data :
+	if len(d) < 100:
+		new.append(d)
+
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+
